4f2010893183d037be25689431eeedb1837df8e0
kaayDev <jugalrajshrestha8@gmail.com>
2023-02-28 20:12:51 +0545

    working on navigation

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
+
 import { ThemeProvider } from "styled-components/native";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { RestaurantsScreen } from "./src/features/restaurant/screens/restaurants.screen";
 
 import { theme } from "./src/infrastructure/theme/";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+console.log(StatusBar.currentHeight);
+
 export default function App() {
+  let [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  // Prevent rendering until the font has loaded
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
